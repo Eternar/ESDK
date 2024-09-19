74abfc3a8add4e7f70ef2436b5cdf718ee0767f0
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-09-19 15:53:27 +0200

    feat(Json): error handling for serializations

--- Shared/Types/Json.lua
+++ Shared/Types/Json.lua
@@ -17,25 +17,49 @@ function JsonObject:Get(key)
 end
 
 function JsonObject:Serialize(state)
-    return JsonConvert.Serialize(self.Object, state);
+    local result, err = JsonConvert.Serialize(self.Object, state);
+
+    if (err) then
+        error(("Serialization failed: %s"):format(err));
+    end
+
+    return result;
 end
 
 function JsonObject:Deserialize(string, position, null, objectmeta, arraymeta)
-    return JsonConvert.Deserialize(string, position, null, objectmeta, arraymeta);
+    local result, pos, err = JsonConvert.Deserialize(string, position, null, objectmeta, arraymeta);
+
+    if (err) then
+        error(("Deserialization failed: '%s' at position: %d"):format(err, pos));
+    end
+
+    return result;
 end
 
 JsonConvert.Serialize = (function(obj, state)
     local finalState = {};
 
     if (type(state) == "boolean") then
-        finalState = { indent = state, };
-    elseif type(state) == "table" then
+        finalState = { indent = state };
+    elseif (type(state) == "table") then
         finalState = state;
     end
 
-    return json.encode(obj, finalState);
+    local encoded, err = json.encode(obj, finalState);
+
+    if not (encoded) then
+        return nil, err;
+    end
+
+    return encoded;
 end);
 
 JsonConvert.Deserialize = (function(string, position, null, objectmeta, arraymeta)
-    return json.decode(string, position, null, objectmeta, arraymeta)
+    local decoded, pos, err = json.decode(string, position, null, objectmeta, arraymeta);
+
+    if not (decoded) then
+        return nil, err;
+    end
+
+    return decoded, pos;
 end);
