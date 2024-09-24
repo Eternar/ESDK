cf06b85acdfb095329a35907a30fab2c621ae6ae
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-09-24 17:43:24 +0200

    tweak: added `DispatchType` enum

---
+++ Client/Enums/DispatchType.lua
@@ -0,0 +1,19 @@
+DispatchType =
+{
+    Invalid = 0,
+	PoliceAutomobile = 1,
+	PoliceHelicopter = 2,
+	FireDepartment = 3,
+	SwatAutomobile = 4,
+	AmbulanceDepartment = 5,
+	PoliceRiders = 6,
+	PoliceVehicleRequest = 7,
+	PoliceRoadBlock = 8,
+	PoliceAutomobileWaitPulledOver = 9,
+	PoliceAutomobileWaitCruising = 10,
+	Gangs = 11,
+	SwatHelicopter = 12,
+	PoliceBoat = 13,
+	ArmyVehicle = 14,
+	BikerBackup = 15
+};
