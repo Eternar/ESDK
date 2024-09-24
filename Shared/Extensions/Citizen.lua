39aa2881e3bb886e7fc13fc1abc8e776dcd16cf6
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-09-24 19:47:34 +0200

    feat(Citizen): `Yield` method

--- Shared/Extensions/Citizen.lua
+++ Shared/Extensions/Citizen.lua
@@ -5,3 +5,33 @@ Citizen.WrapThread = (function(handler, ...)
         handler(table.unpack(args));
     end);
 end);
+
+Citizen.Yield = (function(cb, errorMessage, timeout)
+    local value = cb();
+
+    if (value ~= nil) then
+        return value;
+    end
+
+    if (timeout) or (timeout == nil) then
+        if (type(timeout) ~= "number") then
+            timeout = 1000;
+        end
+    end
+
+    local start = timeout and GetGameTimer();
+
+    while (value == nil) do
+        Citizen.Wait(0);
+
+        local elapsed = timeout and GetGameTimer() - start;
+
+        if (elapsed) and (elapsed > timeout) then
+            return error(("\"^1%s^0\" (waited ^6%.1f^0 ms)") : format(errorMessage or "Failed to resolve callback", elapsed), 2);
+        end
+
+        value = cb();
+    end
+
+    return value;
+end);
