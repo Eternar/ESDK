8d2aa8b3ef2949c33840f840f6e747c666c1cdbc
KillStr3aK <KillStr3aK@users.noreply.github.com>
2024-09-24 17:43:06 +0200

    feat(Client): implemented `TogglePvP`

--- Client/Types/Client.lua
+++ Client/Types/Client.lua
@@ -4,3 +4,14 @@ Client = class "Client";
 Client.GetUpdateChannel = (function()
     return ConVar.GetString("ui_updateChannel", "<unknown>");
 end);
+
+Client.TogglePvP = (function(state, ped)
+    NetworkSetFriendlyFireOption(state);
+    SetRelationshipBetweenGroups(state and GroupRelationship.Hate or GroupRelationship.Companion, "PLAYER", "PLAYER");
+
+    if (ped) then
+        SetCanAttackFriendly(ped, state, false);
+    end
+
+    Console.Log(("PvP has been %s") : format(state and "^2enabled^0" or "^1disabled^0"));
+end);
